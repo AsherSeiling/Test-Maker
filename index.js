441eab04efa96b55b92066b53e5f4d8ea99d14f9
Asher Seiling <f14asher@gmail.com>
2021-03-03 22:11:29 -0600

    Ensuring that all of text boxes work correctly

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ let questions_id = [];
 let answers_id = [];
 
 // Function to handel the creation of new questions
-let questions_counter = 1;
+let questions_counter = 0;
 let questions_html = "";
 function question_load(){
-    questions_html += "<div id=\"question" + questions_counter.toString() + "\" class=\"question\"><p>Question " + questions_counter.toString() + "</p><textarea name=\"question" + questions_counter.toString() + " \"id=\"inquestion" + questions_counter.toString() + "\" cols=\"30\" rows=\"1\" placeholder=\"Put Question here\" oninput=\"textarea_update(" + questions_counter.toString() + ");\"></textarea><br><textarea name=\"answer" + questions_counter.toString() + "\"id=\"answer" + questions_counter.toString() + "\" cols=\"30\" rows=\"1\" placeholder=\"Answer to question here\" oninput=\"textarea_updatean(" + questions_counter.toString() + ");\"></textarea><br><br><button onclick=\"question_load(" + questions_counter.toString() + ");\">New Question</button></div><br>"
+    questions_html += "<div id=\"question" + questions_counter.toString() + "\" class=\"question\"><p>Question " + (questions_counter + 1).toString() + "</p><textarea name=\"question" + questions_counter.toString() + " \"id=\"inquestion" + questions_counter.toString() + "\" cols=\"30\" rows=\"1\" placeholder=\"Put Question here\" oninput=\"textarea_update(" + questions_counter.toString() + ");\"></textarea><br><textarea name=\"answer" + questions_counter.toString() + "\"id=\"answer" + questions_counter.toString() + "\" cols=\"30\" rows=\"1\" placeholder=\"Answer to question here\" oninput=\"textarea_updatean(" + questions_counter.toString() + ");\"></textarea><br><br><button onclick=\"question_load(" + questions_counter.toString() + ");\">New Question</button></div><br>"
     $("#input_questions").html(questions_html);
-    questions_counter += 1;
     window.scrollTo(0,document.body.scrollHeight);
     questions_id.push("inquestion" + questions_counter.toString());
     answers_id.push("answer" + questions_counter.toString());
+    questions_counter += 1;
 };
 
 
@@ -19,7 +19,7 @@ function question_load(){
 let margin = 30;
 let rows1 = 1;
 function textarea_update(id){
-    let id1 = questions_id[id];
+    let id1 = questions_id[parseInt(id)];
     let text1 = $("#" + id1).val();
     if (text1.length >= margin){
         margin += 35;
